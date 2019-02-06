830b57b25647a6a84ae9589f36e2bc2c677d0c50
njunhao <32132659+njunhao@users.noreply.github.com>
2019-02-06 20:19:28 +0800

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /workspace/ROS/tiago_ws/devel/setup.bash --extend
+source /workspace/ROS/ROSPlan/devel/setup.bash --extend
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/workspace/Planners/rex-d/build/src/prada/src/prada/lib/
